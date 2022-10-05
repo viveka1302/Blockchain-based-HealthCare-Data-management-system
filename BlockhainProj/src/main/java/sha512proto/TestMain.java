76d88258a41521cadb4175d461aa8af9aafeeaa8
viveka1302 <vivekanandyt@gmail.com>
2022-10-05 20:54:39 +0530

    implemented and tested object to binary conversion of data for hashing prototype

---
+++ BlockhainProj/src/main/java/sha512proto/TestMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sha512proto;
+
+/**
+ *
+ * @author VIVEK
+ */
+public class TestMain {
+    public static void main(String args[]){
+        binserFileGenerator bsfg= new binserFileGenerator();
+        bsfg.serailize();
+    }
+}
