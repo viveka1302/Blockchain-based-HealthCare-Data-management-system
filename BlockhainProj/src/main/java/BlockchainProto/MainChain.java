e55386bd8d24437c74e94d4c79bf0f4612077e75
viveka1302 <vivekanandyt@gmail.com>
2022-10-15 20:06:10 +0530

    Blockchain prototyping with hashcode almost done

---
+++ BlockhainProj/src/main/java/BlockchainProto/MainChain.java
@@ -0,0 +1,20 @@
+
+package BlockchainProto;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author VIVEK
+ */
+public class MainChain {
+   public static void main(){
+    HashMap<String,Object[]> dataChain;
+       dataChain = new HashMap<String,Object[]>();
+    String[] med1={"120","90","100"};
+    Block genesisBlock;
+       genesisBlock = new Block(0, 1, "Vivek", 19, med1);
+       
+    
+   }
+}
