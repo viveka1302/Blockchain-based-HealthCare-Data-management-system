70bf5eabd6e080c8fc3144a6e54ecf546cda4c09
viveka1302 <vivekanandyt@gmail.com>
2022-11-04 19:22:42 +0530

    Initialised Spring boot

---
+++ HealthChain/HealthChain/src/main/java/com/health/blockchainproj/HealthChain/HealthChainApplication.java
@@ -0,0 +1,13 @@
+package com.health.blockchainproj.HealthChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthChainApplication.class, args);
+	}
+
+}
